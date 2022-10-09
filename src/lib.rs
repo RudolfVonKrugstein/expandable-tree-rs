24a34182f764d9383865f4e4974f4884b7d19f4b
Nathan Hüsken <nathan@wintercloud.de>
2022-10-09 09:59:53 +0200

    [impl] expose RefFlatTree and more type aliases

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ mod values;
 
 pub use tree::Builder;
 pub use tree::FlatTree;
+pub use tree::RefFlatTree;
+
+pub type FlatTreeVec<A> = FlatTree<Vec<A>>;
+pub type RefFlatTreeVec<'a, A> = RefFlatTree<'a, Vec<A>>;
 
 #[cfg(test)]
 mod tests;
