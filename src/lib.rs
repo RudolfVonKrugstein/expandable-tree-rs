dc339870f68e16205c9c479909ebd16af8f7c91b
Nathan Hüsken <nathan@wintercloud.de>
2022-10-09 10:08:26 +0200

    [impl] expose more types to use flat trees directly

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,12 @@ pub use tree::Builder;
 pub use tree::FlatTree;
 pub use tree::RefFlatTree;
 
-pub type FlatTreeVec<A> = FlatTree<Vec<A>>;
-pub type RefFlatTreeVec<'a, A> = RefFlatTree<'a, Vec<A>>;
+pub type FlatTree1<A> = FlatTree<Vec<A>>;
+pub type FlatTree2<A, B> = FlatTree<values::Zip2Values<A, B>>;
+pub type FlatTree3<A, B, C> = FlatTree<values::Zip3Values<A, B, C>>;
+pub type RefFlatTree1<'a, A> = RefFlatTree<'a, Vec<A>>;
+pub type RefFlatTree2<'a, A, B> = RefFlatTree<'a, values::Zip2Values<A, B>>;
+pub type RefFlatTree3<'a, A, B, C> = RefFlatTree<'a, values::Zip3Values<A, B, C>>;
 
 #[cfg(test)]
 mod tests;
